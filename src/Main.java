61a345c712eac3814cd79bfe1a11650b877e3cd5
Joakim Iversen <jiver12@student.aau.dk>
2017-05-21 15:20:27 +0200

    clean up, null fix

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import javax.swing.JFileChooser;
-import javax.swing.JFrame;
 import javax.swing.Timer;
 
 import org.eclipse.swt.SWT;
@@ -26,14 +20,15 @@ import libsvm.svm_model;
 import org.eclipse.wb.swt.SWTResourceManager;
 
 public class Main {
-	
+
 	private final static String ATTENTIVE = "ATTENTIVE";
 	private final static String INATTENTIVE = "INATTENTIVE";
-	
+
 	protected static Shell shell;
 	private static Button btnStart;
 	private static Button btnStop;
 	private static Label lblsamplesCollected;
+	private static Label lblNewData;
 
 	public static Boolean collectingBool = false;
 	private static Label lblData;
@@ -46,36 +41,30 @@ public class Main {
 	static ArrayList<double[]> buildData = reader.getParsedData();
 	static SVMTrainer trainer = new SVMTrainer();
 	static svm_model model = trainer.svmTrain(buildData);
-	
+
 	static DataReader reader2 = new DataReader("src/data/MereTest/builddataLeft.csv");
 	static ArrayList<double[]> buildDataLeft = reader2.getParsedData();
 	static svm_model modelLeft = trainer.svmTrain(buildDataLeft);
-	
+
 	static BluetoothClient bluetoothClient;
-	
+
 	static JFileChooser fc;
-	
-	
 
 	public static void main(String[] args) {
 		System.out.println("\n\n\n\n");
 		System.out.println(false);
-		
+
 		// Initialise bluetooth connection
-		/*bluetoothClient = new BluetoothClient();
-		try {
-			bluetoothClient.initialise();
-			open();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		*/
-		
+		/*
+		 * bluetoothClient = new BluetoothClient(); try {
+		 * bluetoothClient.initialise(); open(); } catch (IOException e) {
+		 * e.printStackTrace(); }
+		 */
+
 		open();
-		
-		//testSampleSet();
-		//testSampleSetLeft();
+
+		// testSampleSet();
+		// testSampleSetLeft();
 	}
 
 	public static void open() {
@@ -89,7 +78,7 @@ public class Main {
 			}
 		}
 	}
-	
+
 	protected static void createContents() {
 		shell = new Shell();
 		shell.setSize(400, 314);
@@ -118,7 +107,7 @@ public class Main {
 				timer.stop();
 			}
 		});
-		
+
 		Button btnCalibrate = new Button(shell, SWT.BUTTON1);
 		btnCalibrate.setText("Calibrate");
 		btnCalibrate.setBounds(274, 155, 100, 34);
@@ -128,7 +117,7 @@ public class Main {
 				dataCollection.open();
 			}
 		});
-		
+
 		Button btnLoadRight = new Button(shell, SWT.BUTTON1);
 		btnLoadRight.setText("Right dataset");
 		btnLoadRight.setBounds(274, 193, 100, 34);
@@ -137,23 +126,26 @@ public class Main {
 				// Load data from a file, and use it for the right model
 				FileChooser fc = new FileChooser();
 				File file = fc.openFile();
-				
-				System.out.println(file.getPath());
-				
-				DataReader readertest = new DataReader(file.getPath());
-				ArrayList<double[]> buildDatatest = readertest.getParsedData();
-				
-				/*for (double[] d : buildDatatest) {
-					for (double db : d) {
-						System.out.print(db + ",");
-					}
-					System.out.println();
-				}*/
-				
-				model = trainer.svmTrain(buildDatatest);
+
+				if (file != null) {
+					System.out.println(file.getPath());
+
+					DataReader readertest = new DataReader(file.getPath());
+					ArrayList<double[]> buildDatatest = readertest.getParsedData();
+
+					/*
+					 * for (double[] d : buildDatatest) { for (double db : d) {
+					 * System.out.print(db + ","); } System.out.println(); }
+					 */
+
+					samplesCollected.setText("");
+					model = trainer.svmTrain(buildDatatest);
+				} else {
+					samplesCollected.setText("file is null");
+				}
 			}
 		});
-		
+
 		Button btnLoadLeft = new Button(shell, SWT.BUTTON1);
 		btnLoadLeft.setText("Left dataset");
 		btnLoadLeft.setBounds(274, 231, 100, 34);
@@ -162,13 +154,23 @@ public class Main {
 				// Load data from a file, and use it for the right model
 				FileChooser fc = new FileChooser();
 				File file = fc.openFile();
-				
-				System.out.println(file.getPath());
-				
-				DataReader readertest = new DataReader(file.getPath());
-				ArrayList<double[]> buildDatatest = readertest.getParsedData();
-				
-				modelLeft = trainer.svmTrain(buildDatatest);
+
+				if (file != null) {
+					System.out.println(file.getPath());
+
+					DataReader readertest = new DataReader(file.getPath());
+					ArrayList<double[]> buildDatatest = readertest.getParsedData();
+
+					/*
+					 * for (double[] d : buildDatatest) { for (double db : d) {
+					 * System.out.print(db + ","); } System.out.println(); }
+					 */
+
+					samplesCollected.setText("");
+					modelLeft = trainer.svmTrain(buildDatatest);
+				} else {
+					samplesCollected.setText("file is null");
+				}
 			}
 		});
 
@@ -176,7 +178,7 @@ public class Main {
 		lblsamplesCollected.setBounds(10, 101, 100, 21);
 		lblsamplesCollected.setText("Samples collected");
 
-		Label lblNewData = new Label(shell, SWT.NONE);
+		lblNewData = new Label(shell, SWT.NONE);
 		lblNewData.setText("Newest data");
 		lblNewData.setBounds(10, 10, 86, 15);
 
@@ -196,13 +198,13 @@ public class Main {
 		timer = new Timer(250, timerAction);
 		timer.start();
 	}
-	
-	// Testing 
+
+	// Testing
 	public static void testSampleSet() {
 		SVMTrainer trainer = new SVMTrainer();
 		DataReader reader2 = new DataReader("src/data/MereTest/testdata.csv");
 		ArrayList<double[]> testData = reader2.getParsedData();
-		
+
 		int notConfidentEnough = 0;
 		int numberOfResting = 0;
 		int numberOfSteering = 0;
@@ -213,7 +215,7 @@ public class Main {
 		int numberOfSecondaryCorrect = 0;
 		int numberOfGearstickCorrect = 0;
 		int numberOfFalse = 0;
-		
+
 		for (double[] d : testData) {
 			double predicted = trainer.svmPredict(d, model);
 
@@ -225,8 +227,7 @@ public class Main {
 					notConfidentEnough++;
 				else
 					numberOfFalse++;
-			}
-			else if (d[0] == 2.0) {
+			} else if (d[0] == 2.0) {
 				numberOfResting++;
 				if (predicted == 2.0)
 					numberOfRestingCorrect++;
@@ -234,8 +235,7 @@ public class Main {
 					notConfidentEnough++;
 				else
 					numberOfFalse++;
-			}
-			else if (d[0] == 3.0) {
+			} else if (d[0] == 3.0) {
 				numberOfSecondary++;
 				if (predicted == 3.0)
 					numberOfSecondaryCorrect++;
@@ -243,8 +243,7 @@ public class Main {
 					notConfidentEnough++;
 				else
 					numberOfFalse++;
-			}
-			else if (d[0] == 4.0) {
+			} else if (d[0] == 4.0) {
 				numberOfGearstick++;
 				if (predicted == 4.0)
 					numberOfGearstickCorrect++;
@@ -254,32 +253,33 @@ public class Main {
 					numberOfFalse++;
 			}
 		}
-		
+
 		System.out.println("Final results");
 		System.out.println("Number of not confident enough: " + notConfidentEnough);
 		System.out.println("Number of steering: " + numberOfSteeringCorrect + "/" + numberOfSteering);
 		System.out.println("Number of resting: " + numberOfRestingCorrect + "/" + numberOfResting);
 		System.out.println("Number of secondary/communication: " + numberOfSecondaryCorrect + "/" + numberOfSecondary);
 		System.out.println("Number of gearstick: " + numberOfGearstickCorrect + "/" + numberOfGearstick);
-		System.out.println("Number of false predictions: " + numberOfFalse + "/" + (numberOfResting + numberOfSteering + numberOfSecondary + numberOfGearstick));
+		System.out.println("Number of false predictions: " + numberOfFalse + "/"
+				+ (numberOfResting + numberOfSteering + numberOfSecondary + numberOfGearstick));
 		System.out.println();
 	}
-	
+
 	public static void testSampleSetLeft() {
 		SVMTrainer trainer = new SVMTrainer();
 		DataReader reader2 = new DataReader("src/data/MereTest/testdataLeft.csv");
 		ArrayList<double[]> testData = reader2.getParsedData();
-		
+
 		int numberOfNone = 0;
 		int numberOfResting = 0;
 		int numberOfSteering = 0;
 		int numberOfRestingCorrect = 0;
 		int numberOfSteeringCorrect = 0;
 		int numberOfFalse = 0;
-		
+
 		for (double[] d : testData) {
 			double predicted = trainer.svmPredictLeft(d, modelLeft);
-			
+
 			if (d[0] == 1.0) {
 				numberOfSteering++;
 				if (predicted == 1.0)
@@ -288,8 +288,7 @@ public class Main {
 					numberOfNone++;
 				else
 					numberOfFalse++;
-			}
-			else if (d[0] == 2.0) {
+			} else if (d[0] == 2.0) {
 				numberOfResting++;
 				if (predicted == 2.0)
 					numberOfRestingCorrect++;
@@ -299,15 +298,16 @@ public class Main {
 					numberOfFalse++;
 			}
 		}
-		
+
 		System.out.println("Final results left");
 		System.out.println("number of not confident enough: " + numberOfNone);
 		System.out.println("Number of steering correct: " + numberOfSteeringCorrect + "/" + numberOfSteering);
 		System.out.println("Number of resting correct: " + numberOfRestingCorrect + "/" + numberOfResting);
-		System.out.println("number of false predictions: " + numberOfFalse + "/" + (numberOfResting + numberOfSteering));
+		System.out
+				.println("number of false predictions: " + numberOfFalse + "/" + (numberOfResting + numberOfSteering));
 		System.out.println();
 	}
-	
+
 	public static double[] getSample(int handNumber) {
 		Controller controller = new Controller();
 		Frame frame = controller.frame();
@@ -316,31 +316,30 @@ public class Main {
 		if (frame.hands().count() >= 1) {
 			Hand hand = frame.hands().get(handNumber);
 
-			temp = new double[] { 
-					0.0, 
+			temp = new double[] { 0.0,
 					// Hand
-					hand.sphereCenter().normalized().getX(),
+					hand.sphereCenter().normalized().getX(), 
 					hand.sphereCenter().normalized().getY(),
-					hand.sphereCenter().normalized().getZ(),
+					hand.sphereCenter().normalized().getZ(), 
 					hand.grabStrength(), 
-					hand.pinchStrength(), 
+					hand.pinchStrength(),
 					// Palm
-					hand.palmNormal().getX(),
+					hand.palmNormal().getX(), 
 					hand.palmNormal().getY(), 
-					hand.palmNormal().getZ(), 
+					hand.palmNormal().getZ(),
 					hand.stabilizedPalmPosition().normalized().getX(),
 					hand.stabilizedPalmPosition().normalized().getY(),
 					hand.stabilizedPalmPosition().normalized().getZ(),
 					// Direction
-					hand.direction().getX(),
+					hand.direction().getX(), 
 					hand.direction().getY(), 
 					hand.direction().getZ(), 
-					hand.direction().pitch(), 
-					hand.direction().roll(),
-					hand.direction().yaw(), 
+					hand.direction().pitch(),
+					hand.direction().roll(), 
+					hand.direction().yaw(),
 					// Fingers
-					hand.fingers().get(0).direction().getX(),
-					hand.fingers().get(0).direction().getY(), 
+					hand.fingers().get(0).direction().getX(), 
+					hand.fingers().get(0).direction().getY(),
 					hand.fingers().get(0).direction().getZ(),
 					hand.fingers().get(0).stabilizedTipPosition().normalized().getX(),
 					hand.fingers().get(0).stabilizedTipPosition().normalized().getY(),
@@ -368,8 +367,7 @@ public class Main {
 					hand.fingers().get(4).direction().getZ(),
 					hand.fingers().get(4).stabilizedTipPosition().normalized().getX(),
 					hand.fingers().get(4).stabilizedTipPosition().normalized().getY(),
-					hand.fingers().get(4).stabilizedTipPosition().normalized().getZ(), 
-					};
+					hand.fingers().get(4).stabilizedTipPosition().normalized().getZ(), };
 
 		}
 		return temp;
